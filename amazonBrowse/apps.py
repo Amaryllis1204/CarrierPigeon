78888923f3e8851d6133e67e590358972615a122
Amaryllis1204 <amaryllis1204@gmail.com>
2021-11-24 15:22:19 +0900

    入力フォームの表示に成功

---
+++ amazonBrowse/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class amazonBrowseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'amazonBrowse'
---
+++ amazonBrowse/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# application/write_data.pyをインポートする
+from .application import write_data
+
+# Create your views here.
+def index(req):
+    return render(req, 'amazonBrowse/index.html')
+
+# ajaxでurl指定したメソッド
+def call_write_data(req):
+    if req.method == 'GET':
+        # write_data.pyのwrite_csv()メソッドを呼び出す。
+        # ajaxで送信したデータのうち"input_data"を指定して取得する。
+        write_data.write_csv(req.GET.get("input_data"))
+
+        data = write_data.return_text()
+
+        return HttpResponse(data)
