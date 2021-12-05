7f07cb3e9aedf1e3b4ce2a12f28b26b6ef667ad5
Amaryllis1204 <amaryllis1204@gmail.com>
2021-12-06 00:34:51 +0900

    fix url

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('amazonBrowse/', include('amazonBrowse.urls')),
+    path('^', include('amazonBrowse.urls')),
     path('admin/', admin.site.urls),
 ]
