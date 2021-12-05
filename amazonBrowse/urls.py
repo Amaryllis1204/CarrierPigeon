be164c2c988d681e51b530c012232f5bced6ffd0
Amaryllis1204 <amaryllis1204@gmail.com>
2021-12-06 00:23:52 +0900

    fix url

--- amazonBrowse/urls.py
+++ amazonBrowse/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'amazonBrowse'
 urlpatterns = [
-    path(r'', views.index, name='index'),
+    path(r'index/', views.index, name='index'),
     # 以下を追記(views.pyのcall_write_data()にデータを送信できるようにする)
     # path("ajax/", views.call_write_data, name="call_write_data"),
     path("ajax/", views.update_db, name="update_db")
