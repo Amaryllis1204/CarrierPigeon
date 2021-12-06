b85bd6f816b86d84cc36495c40a12fab38e58107
Amaryllis1204 <amaryllis1204@gmail.com>
2021-12-06 23:47:53 +0900

    fix chrome driver

--- amazonBrowse/application/read_site.py
+++ amazonBrowse/application/read_site.py
@@ -12,13 +12,20 @@ def amazon_only(keyword):
     url = uri + '+' + keyword + "&__mk_ja_JP=カタカナ&ref=nb_sb_noss_2"
     text = ''
     #ヘッドレスモードでブラウザを起動
-    driver_path = '/app/.chromedriver/bin/chromedriver'
-    options = Options()
-    options.add_argument('--headless')
+    # driver_path = '/app/.chromedriver/bin/chromedriver'
+    # options = Options()
+    # options.add_argument('--headless')
     
-    #ブラウザを起動
-    #driver = webdriver.Chrome('/usr/bin/chromedriver')
-    driver = webdriver.Chrome(driver_path)
+    # #ブラウザを起動
+    # #driver = webdriver.Chrome('/usr/bin/chromedriver')
+    # driver = webdriver.Chrome(driver_path)
+
+    driver_path = '/app/.chromedriver/bin/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    #driverに設定 ※optionsを指定しないとheadlessにならないので注意
+    driver = webdriver.Chrome(options=options, executable_path=driver_path)
+
     driver.get(url)
     driver.implicitly_wait(10)
     text = driver.page_source
